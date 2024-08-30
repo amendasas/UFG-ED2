31759e8139e9ad6b4f416d00034ce04404b4fadf
Amanda Paulino <161528490+amendasas@users.noreply.github.com>
2024-08-29 21:10:49 -0300

    Update 02-Matrizes.c

--- RevisãoED1/02-Matrizes.c
+++ RevisãoED1/02-Matrizes.c
@@ -1,24 +1,39 @@
-/*
-O código abaixo recebe do usuário elementos que são inseridos em uma matriz 3x3, em seguida, imprime a soma de sua diagonal principal.
-- faça uma modificação para que seja possível dinamizar o tamanho da matriz (utilizar alocação dinâmica de memória usando malloc)
-*/
-
 #include <stdio.h>
+#include <stdlib.h>  // Necessário para usar malloc
 
 int main() {
-    int matriz[3][3];
-    int i, j, soma = 0;
+    int n, i, j, soma = 0;
+    int **matriz;
 
-    printf("Insira os elementos da matriz 3x3:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    // Solicita ao usuário o tamanho da matriz (n x n)
+    printf("Insira o tamanho da matriz (n x n): ");
+    scanf("%d", &n);
+
+    // Alocação dinâmica de memória para a matriz 2D
+    matriz = (int **)malloc(n * sizeof(int *));
+    for (i = 0; i < n; i++) {
+        matriz[i] = (int *)malloc(n * sizeof(int));
+    }
+
+    // Solicita ao usuário para inserir os elementos da matriz
+    printf("Insira os elementos da matriz %dx%d:\n", n, n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             scanf("%d", &matriz[i][j]);
             if (i == j) {
-                soma += matriz[i][j];
+                soma += matriz[i][j];  // Soma os elementos da diagonal principal
             }
         }
     }
 
+    // Exibe a soma da diagonal principal
     printf("Soma dos elementos da diagonal principal: %d\n", soma);
+
+    // Liberação da memória alocada dinamicamente
+    for (i = 0; i < n; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+
     return 0;
 }
